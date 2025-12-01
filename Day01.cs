16335c080e35fb4ffb420d2e033a702665a3689a
Jonas Buggisch <jonas.buggisch@gmail.com>
2025-12-01 13:19:25 +0100

    feat: finished day 1

---
+++ Day01.cs
@@ -0,0 +1,86 @@
+﻿namespace AdventOfCode;
+
+enum Direction
+{
+  Left,
+  Right
+}
+
+struct Turn(Direction dir, int amount)
+{
+  public Direction dir = dir;
+  public int amount = amount;
+}
+
+public class Day01 : BaseDay
+{
+  private readonly List<Turn> turns = [];
+  private int dial = 50;
+
+  public Day01()
+  {
+    string input = File.ReadAllText(InputFilePath);
+    string[] lines = input.Split([Environment.NewLine], StringSplitOptions.None);
+
+    foreach (var line in lines)
+    {
+      if (line == "")
+        return;
+
+      Direction dir = line[0] == 'L' ? Direction.Left : Direction.Right;
+      int amount = int.Parse(line[1..]);
+      turns.Add(new Turn(dir, amount));
+    }
+  }
+
+  public override ValueTask<string> Solve_1()
+  {
+    dial = 50;
+
+    int result = 0;
+    foreach (var turn in turns)
+    {
+      (int exactZero, int _) = Rotate(turn);
+      result += exactZero;
+    }
+
+    return new(result.ToString());
+  }
+
+  public override ValueTask<string> Solve_2()
+  {
+    dial = 50;
+
+    int result = 0;
+    foreach (var turn in turns)
+    {
+      (int _, int passedZero) = Rotate(turn);
+      result += passedZero;
+    }
+
+    return new(result.ToString());
+  }
+
+  private (int exactZero, int passedZero) Rotate(Turn turn)
+  {
+    int exactZero = 0;
+    int passedZero = 0;
+
+    var step = turn.dir == Direction.Left ? -1 : 1;
+    for (var i = 0; i < turn.amount; i++)
+    {
+      dial = (dial + step + 100) % 100;
+      if (dial == 0)
+      {
+        passedZero++;
+      }
+    }
+
+    if (dial == 0)
+    {
+      exactZero++;
+    }
+
+    return (exactZero, passedZero);
+  }
+}
---
+++ Day_26.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode;
+
+public class Day_26 : BaseDay
+{
+  public Day_26()
+  {
+    string _input = File.ReadAllText(InputFilePath);
+  }
+
+  public override ValueTask<string> Solve_1()
+  {
+    return new("0");
+  }
+
+  public override ValueTask<string> Solve_2()
+  {
+    return new("0");
+  }
+}
